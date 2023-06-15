b8a22cd0ddfb9a66ac761d3ee7c5f021f79bdb55
cute90210 <marceloah9199@gmail.com>
2023-06-15 13:35:12 -0400

    feat: add basic functionality of current weather to base styling on

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-const search = document.querySelector("[data-location-form]")
+const currentIcon = document.querySelector("[data-current-icon]");
+const currentTemp = document.querySelector("[data-current-temp]");
+const search = document.querySelector("[data-location-form]");
+let locationInput = document.querySelector("[data-location-input]");
 
-async function getWeather () {
-  
-  let response = await fetch("https://api.tomorrow.io/v4/weather/forecast?location=miami&timesteps=&units=metric&apikey=HKIjDqRqtwQNfMfZowUC78M6HCZocaCQ")
-  let getData = await response.json();
-  
+(async function getCurrentWeather () {
+  let location = locationInput.value
+  try {
+    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=47c174966a23453c98f23538230506&q=London&days=7&aqi=no&alerts=no`)
+    let getData = await response.json();
+    renderCurrentWeatherIcon(getData);
+    renderCurrentTemp(getData)
+    console.log(getData)
+  } catch(error){
+    console.error("sorry man", error)
+  }
+ 
+})()
 
+function renderCurrentWeatherIcon (getData) {
+  currentIcon.src = getData.current.condition.icon
 }
 
-search.addEventListener("submit", ()=>{
-    
+function renderCurrentTemp (getData) {
+  currentTemp.innerText = getData.current.temp_f + "°F"
+}
+
+search.addEventListener("submit", (e)=>{
+  e.preventDefault();
+    getCurrentWeather();
 })
+
+
+
