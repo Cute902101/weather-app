b5f162847c00ba2814ac9e7f8e8c6a7d1d6183d7
cute90210 <marceloah9199@gmail.com>
2023-06-18 09:34:33 -0400

    feat: add hourly forecast data to DOMelements

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ let locationInput = document.querySelector("[data-location-input]");
 async function fetchWeatherData () {
   let location = locationInput.value
   try {
-    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=47c174966a23453c98f23538230506&q=Miami&days=7&aqi=no&alerts=no`)
+    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=47c174966a23453c98f23538230506&q=Homestead&days=7&aqi=no&alerts=no`)
     let apiData = await response.json();
     apiDataModule.setApiData(apiData);
     apiDataModule.consoleData()
@@ -27,4 +27,56 @@ search.addEventListener("submit", (e)=>{
     fetchWeatherData();
 })})()
 
+
+function createHourlyElements() {
+  const parentDiv = document.querySelector(".hourly-container");
+
+  for (let i = 0; i < 24; i++) {
+    // Create the main container div
+    const hourlyDiv = document.createElement("div");
+    hourlyDiv.className = "hourly p-4";
+    hourlyDiv.textContent = "hour";
+
+    // Create the grid div
+    const gridDiv = document.createElement("div");
+    gridDiv.className = "grid p-4";
+
+    // Create the image element
+    const img = document.createElement("img");
+    img.className = "hourly-condition-img";
+    img.src = "#";
+    gridDiv.appendChild(img);
+
+    // Create the condition heading
+    const conditionHeading = document.createElement("h1");
+    conditionHeading.className = "hourly-condition";
+    conditionHeading.textContent = "(condition)";
+    gridDiv.appendChild(conditionHeading);
+
+    // Create the precipitation paragraph
+    const precipitationPara = document.createElement("p");
+    precipitationPara.className = "hourly-precipitaion";
+    precipitationPara.textContent = "(precipitation)";
+    gridDiv.appendChild(precipitationPara);
+
+    // Create the humidity paragraph
+    const humidityPara = document.createElement("p");
+    humidityPara.className = "hourly-humidity";
+    humidityPara.textContent = "(humidity)";
+    gridDiv.appendChild(humidityPara);
+
+    // Create the wind paragraph
+    const windPara = document.createElement("p");
+    windPara.className = "hourly-wind";
+    windPara.textContent = "(wind)";
+    gridDiv.appendChild(windPara);
+
+    // Append the grid div to the hourly div
+    hourlyDiv.appendChild(gridDiv);
+
+    // Append the hourly div to the parent div
+    parentDiv.appendChild(hourlyDiv);
+  }
+}
+createHourlyElements()
 
